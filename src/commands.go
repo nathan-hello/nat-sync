1e6195cbf0452f2ca2401bd2fff3928f43a0a275
nathan-hello <natey3k@gmail.com>
2024-06-26 23:00:32 -0400

    BINARY ENCODING BABY

--- src/commands.go
+++ src/commands.go
@@ -1,59 +1,93 @@
 package src
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"time"
+
+	"github.com/nathan-hello/nat-sync/src/utils"
 )
 
 const CurrentVersion = "0.0.1"
 
-func MarshalErrToJSON(raw any, err error) string {
-	return fmt.Sprintf(`{error: "%s", raw_command: "%s" }`, err.Error(), raw)
+// Command:
+//	seek:      0000_0001
+//	pause:     0000_0010
+//	play:      0000_0011
+//	new_video: 0000_0100
 
+type Command struct {
+	Command       uint8
+	Version       uint8
+	Creator       uint8 // map[uint8]string for mapping bits to usernames
+	ContentLength uint8
+	Content       Renderer
 }
 
-type Command interface {
-	Render() (RenderedCommand, error)
+type Renderer interface{ Render() (*Command, error) }
+
+func EncodeCommand(cmd Command) ([]byte, error) {
+	if cmd.Content == nil {
+		return nil, utils.ErrNoContent
+	}
+	bits := bytes.Buffer{}
+
+	err := binary.Write(&bits, binary.LittleEndian, cmd.Command)
+	if err != nil {
+		return nil, utils.ByteEncodingErr(bits)
+	}
+	err = binary.Write(&bits, binary.LittleEndian, 0000_0001)
+	if err != nil {
+		return nil, utils.ByteEncodingErr(bits)
+	}
+	err = binary.Write(&bits, binary.LittleEndian, cmd.Creator)
+	if err != nil {
+		return nil, utils.ByteEncodingErr(bits)
+	}
+	err = binary.Write(&bits, binary.LittleEndian, len(cmd.Content))
+	if err != nil {
+		return nil, utils.ByteEncodingErr(bits)
+	}
+	err = binary.Write(&bits, binary.LittleEndian, cmd.Content)
+	if err != nil {
+		return nil, utils.ByteEncodingErr(bits)
+	}
+
+	return nil, nil
 }
 
-type RenderedCommand struct {
-	Command string      `json:"command"`
-	Version string      `json:"version"`
-	Content interface{} `json:"content"`
+func DecodeCommand(bits []byte) (*Command, error) {
+	return nil, nil
 }
 
 type Seek struct {
-	Creator  string `json:"creator"`
-	Location string `json:"difference"`
+	Location string
 }
 
-func (c *Seek) Render() (*RenderedCommand, error) {
+func (c *Seek) Render() (*Command, error) {
 	t, err := time.ParseDuration(c.Location)
 	if err != nil {
 		return nil, err
 	}
 	c.Location = fmt.Sprintf("%.0f", t.Seconds())
-	return &RenderedCommand{Command: "seek", Version: CurrentVersion, Content: c}, nil
+	return &Command{Command: 0000_0001, Content: c}, nil
 }
 
-type Pause struct {
-	Creator string `json:"creator"`
+type Pause struct{}
+
+func (c *Pause) Render() (*Command, error) {
+	return &Command{Command: 0000_0010, Content: c}, nil
 }
 
-func (c *Pause) Render() (*RenderedCommand, error) {
-	return &RenderedCommand{Command: "pause", Version: CurrentVersion, Content: c}, nil
-}
+type Play struct{}
 
-type Play struct {
-	Creator string `json:"creator"`
-}
-
-func (c *Play) Render() (*RenderedCommand, error) {
-	return &RenderedCommand{Command: "play", Version: CurrentVersion, Content: c}, nil
+func (c *Play) Render() (*Command, error) {
+	return &Command{Command: 0000_0100, Content: c}, nil
 }
 
 type NewVideo struct {
-	Creator string `json:"creator"`
-	Uri     string `json:"uri"`
-	Local   bool   `json:"local"`
+	Creator string
+	Uri     string
+	Local   bool
 }
--- src/utils/errors.go
+++ src/utils/errors.go
@@ -1,8 +1,17 @@
 package utils
 
-import "errors"
+import (
+	"bytes"
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrInvalidArg = errors.New("command was given invalid arguments")
 	ErrInvalidCmd = errors.New("command given to server is not valid")
+	ErrNoContent  = errors.New("command given has no content")
 )
+
+func ByteEncodingErr(buf bytes.Buffer) error {
+	return fmt.Errorf("could not encode the following bytes: %b\nstring representation: %s", buf, buf.String())
+}
